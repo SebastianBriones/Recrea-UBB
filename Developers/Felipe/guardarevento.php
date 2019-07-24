512d93f3cfca716a46278fb779e382f946b47f18
FelipeSebastianEspinoza <sebastian_felipe1507@hotmail.com>
2019-07-24 12:12:27 -0400

    Modificacion para los nuevos cambios en la base de datos y nueva funcion para insertar tildes

--- Developers/Felipe/guardarevento.php
+++ Developers/Felipe/guardarevento.php
@@ -9,7 +9,7 @@
 	private $EVE_DESCRIP;
  
     function __construct($bd){
-	    $this->con = new mysqli('localhost','recreaubb','recrea2019',$bd);
+	    $this->con = new mysqli('localhost','recreaubb','recrea2019',$bd); 
 	}
 	
     function insertar($form_data){
@@ -17,20 +17,27 @@
 		$nombre_archivo=($_FILES['ARCHIVO']['name']);
         $tipo_archivo=($_FILES['ARCHIVO']['type']);
         $imageData = addslashes(file_get_contents($_FILES['ARCHIVO']['tmp_name']));
- 
+		
         $CAT_CORREL = $_POST['CAT_CORREL'];
-		$EVE_NOMBRE = $_POST['EVE_NOMBRE'];
+		$EVE_NOMBRE = htmlentities($_POST['EVE_NOMBRE']);
 		$EVE_FECHA = $_POST['EVE_FECHA'];
 		$EVE_HORA = $_POST['EVE_HORA'];
-		$EVE_DESCRIP = $_POST['EVE_DESCRIP'];
-        $consulta = "INSERT INTO `EVENTO` (`EVE_CORREL`,`CAT_CORREL`,`EVE_NOMBRE`,
-		`EVE_FECHA`,`EVE_HORA`,`EVE_DESCRIP`,`EVE_NOTA`,`EVE_ESTADO`,`IMAGEN`) VALUES 
-		(NULL,'$CAT_CORREL','$EVE_NOMBRE','$EVE_FECHA',
-		'$EVE_HORA','$EVE_DESCRIP',NULL,NULL,'$imageData');";				
+		$EVE_DESCRIP =htmlentities($_POST['EVE_DESCRIP']);
+
+		
+		
+        $consulta = "INSERT INTO `EVENTO` (`EVE_CORREL`,`CAT_CORREL`,`EST_CORREL`,`EVE_NOMBRE`,
+		`EVE_FECHA`,`EVE_HORA`,`EVE_DESCRIP`,`EVE_NOTA`,`IMAGEN`) VALUES 
+		(NULL,'$CAT_CORREL',NULL,'$EVE_NOMBRE','$EVE_FECHA',
+		'$EVE_HORA','$EVE_DESCRIP',NULL,'$imageData');";
+
+
+		
         $resultado_cons = mysqli_query($this->con,$consulta);
+		
         if($resultado_cons == false){
 			echo "<script> 
-					//alert('Error en los datos');
+					 alert('Error en los datos');
 				  </script>";
 		}else{
 			echo "<script>
