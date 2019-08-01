d8e4f439ad08a817f188a9e3184925668791ef1b
SebastianBriones <sebastian.briones1401@alumnos.ubiobio.cl>
2019-08-01 03:55:05 -0400

    Modifica mostrareventos, muestramiseventos y muestraparticipa

--- Developers/Sebastian/mostrareventos.php
+++ Developers/Sebastian/mostrareventos.php
@@ -16,6 +16,18 @@
 									   INNER JOIN EDIFICIO D ON S.EDI_CORREL = D.EDI_CORREL
 									   WHERE E.EST_CORREL = 1');
 
+	//En caso contrario se muestran los eventos de la categoria especificada y que esten disponibles
+	}else{
+		$result = mysqli_query($conn, 'SELECT *
+								  	   FROM EVENTO E
+								  	   INNER JOIN REALIZA R ON E.EVE_CORREL = R.EVE_CORREL
+									   INNER JOIN ESPACIO S ON R.ESP_CORREL = S.ESP_CORREL
+									   INNER JOIN EDIFICIO D ON S.EDI_CORREL = D.EDI_CORREL
+								       WHERE E.EST_CORREL = 1
+								         AND E.CAT_CORREL = '.$value.';');
+
+	}
+
 		while($row = mysqli_fetch_array($result)){
 		?>
 			<div class="row">
@@ -33,34 +45,6 @@
 			</div>
 		<?php
 		}
-	//En caso contrario se muestran los eventos de la categoria especificada y que esten disponibles
-	}else{
-		$result = mysqli_query($conn, 'SELECT *
-								  	   FROM EVENTO E
-								  	   INNER JOIN REALIZA R ON E.EVE_CORREL = R.EVE_CORREL
-									   INNER JOIN ESPACIO S ON R.ESP_CORREL = S.ESP_CORREL
-									   INNER JOIN EDIFICIO D ON S.EDI_CORREL = D.EDI_CORREL
-								       WHERE E.EST_CORREL = 1
-								         AND E.CAT_CORREL = '.$value.';');
-
-		while($row = mysqli_fetch_array($result)){
-		?>
-			<div class="row">
-				<div class="col-6 col-md-4 col-lg-3 col-x1-2 js-col-evento">
-					<a href="verevento.php?value=<?php echo $row["EVE_CORREL"] ?>" class="evento" id="<?php echo $row["EVE_CORREL"]; ?>">
-						<?php echo '<img style="width:250px;height:210px;" src="data:image/png;base64,'.base64_encode( $row["IMAGEN"] ).'"/>';?>
-						<div class="titulo">
-							<h5><?php echo $row["EVE_NOMBRE"] ?></h5>
-							<p><?php echo $row["ESP_NOMBRE"] ?></p> 
-							<P><?php echo $row ["EDI_NOMBRE"] ?></p>
-							<p><?php echo $row["EVE_FECHA"] ?></p>
-						</div>
-					</a>
-				</div>
-			</div>
-		<?php
-		}
-	}
 
 	include("desconexion.php");
 ?>
--- Developers/Sebastian/muestraparticipa.php
+++ Developers/Sebastian/muestraparticipa.php
@@ -15,102 +15,54 @@
 
 	//Si los valores recibidos son ambos 0 se muestras todos los eventos de todas las categorias y estados
 	if($value1 == 0 && $value2 == 0){
-		$result = mysqli_query($conn, 'SELECT *
+		$result = mysqli_query($conn, "SELECT *
 									   FROM EVENTO E
 									   INNER JOIN REALIZA R ON E.EVE_CORREL = R.EVE_CORREL
 									   INNER JOIN ESPACIO S ON R.ESP_CORREL = S.ESP_CORREL
 									   INNER JOIN EDIFICIO D ON S.EDI_CORREL = D.EDI_CORREL
 									   INNER JOIN ASISTE A ON E.EVE_CORREL = A.EVE_CORREL
 									   INNER JOIN USUARIO U ON A.USU_RUT = U.USU_RUT
-									   WHERE U.USU_RUT = '.$rut.';');
+									   WHERE U.USU_RUT = '".$rut."';");
 
-		while($row = mysqli_fetch_array($result)){
-		?>
-			<div class="row">
-				<div class="col-6 col-md-4 col-lg-3 col-x1-2 js-col-evento">
-					<a href="verevento.php?value=<?php echo $row["EVE_CORREL"]; ?>" class="evento" id="<?php echo $row["EVE_CORREL"]; ?>">
-						<?php echo '<img style="width:250px;height:210px;" src="data:image/png;base64,'.base64_encode( $row["IMAGEN"] ).'"/>';?>
-						<div class="titulo">
-							<h5><?php echo $row["EVE_NOMBRE"] ?></h5>
-							<p><?php echo $row["ESP_NOMBRE"] ?></p> 
-							<P><?php echo $row ["EDI_NOMBRE"] ?></p>
-							<p><?php echo $row["EVE_FECHA"] ?></p>
-						</div>
-					</a>
-				</div>
-			</div>
-		<?php
-		}
 	//En caso contrario se muestran los eventos de la categoria especificada y que esten disponibles
 	}else if($value1 != 0 && $value2 == 0){
-		$result = mysqli_query($conn, 'SELECT * 
+		$result = mysqli_query($conn, "SELECT * 
 								  	   FROM EVENTO E
 								  	   INNER JOIN REALIZA R ON E.EVE_CORREL = R.EVE_CORREL
 									   INNER JOIN ESPACIO S ON R.ESP_CORREL = S.ESP_CORREL
 									   INNER JOIN EDIFICIO D ON S.EDI_CORREL = D.EDI_CORREL
 									   INNER JOIN ASISTE A ON E.EVE_CORREL = A.EVE_CORREL
 									   INNER JOIN USUARIO U ON A.USU_RUT = U.USU_RUT
-									   WHERE U.USU_RUT = "'.$rut.'"
-								         AND E.CAT_CORREL = '.$value1.';');
+									   WHERE U.USU_RUT = '".$rut."'
+								         AND E.CAT_CORREL = ".$value1.";");
 
-		while($row = mysqli_fetch_array($result)){
-		?>
-			<div class="row">
-				<div class="col-6 col-md-4 col-lg-3 col-x1-2 js-col-evento">
-					<a href="verevento.php?value=<?php echo $row["EVE_CORREL"] ?>" class="evento" id="<?php echo $row["EVE_CORREL"]; ?>">
-						<?php echo '<img style="width:250px;height:210px;" src="data:image/png;base64,'.base64_encode( $row["IMAGEN"] ).'"/>';?>
-						<div class="titulo">
-							<h5><?php echo $row["EVE_NOMBRE"] ?></h5>
-							<p><?php echo $row["ESP_NOMBRE"] ?></p> 
-							<P><?php echo $row ["EDI_NOMBRE"] ?></p>
-							<p><?php echo $row["EVE_FECHA"] ?></p>
-						</div>
-					</a>
-				</div>
-			</div>
-		<?php
-		}
 	//En otro caso se muestran los eventos con el estado especificado y que esten disponibles
 	}else if($value1 == 0 && $value2 != 0){
-		$result = mysqli_query($conn, 'SELECT * 
+		$result = mysqli_query($conn, "SELECT * 
 								  	   FROM EVENTO E
 								  	   INNER JOIN REALIZA R ON E.EVE_CORREL = R.EVE_CORREL
 									   INNER JOIN ESPACIO S ON R.ESP_CORREL = S.ESP_CORREL
 									   INNER JOIN EDIFICIO D ON S.EDI_CORREL = D.EDI_CORREL
 									   INNER JOIN ASISTE A ON E.EVE_CORREL = A.EVE_CORREL
 									   INNER JOIN USUARIO U ON A.USU_RUT = U.USU_RUT
-									   WHERE U.USU_RUT = "'.$rut.'"
-								         AND E.EST_CORREL = '.$value2.';');
+									   WHERE U.USU_RUT = '".$rut."'
+								         AND E.EST_CORREL = ".$value2.";");
 
-		while($row = mysqli_fetch_array($result)){
-		?>
-			<div class="row">
-				<div class="col-6 col-md-4 col-lg-3 col-x1-2 js-col-evento">
-					<a href="verevento.php?value=<?php echo $row["EVE_CORREL"] ?>" class="evento" id="<?php echo $row["EVE_CORREL"]; ?>">
-						<?php echo '<img style="width:250px;height:210px;" src="data:image/png;base64,'.base64_encode( $row["IMAGEN"] ).'"/>';?>
-						<div class="titulo">
-							<h5><?php echo $row["EVE_NOMBRE"] ?></h5>
-							<p><?php echo $row["ESP_NOMBRE"] ?></p> 
-							<P><?php echo $row ["EDI_NOMBRE"] ?></p>
-							<p><?php echo $row["EVE_FECHA"] ?></p>
-						</div>
-					</a>
-				</div>
-			</div>
-		<?php
-		}
 	//Se muestran los eventos con estado y categoria especificada
 	}else{
-		$result = mysqli_query($conn, 'SELECT * 
+		$result = mysqli_query($conn, "SELECT * 
 								  	   FROM EVENTO E
 								  	   INNER JOIN REALIZA R ON E.EVE_CORREL = R.EVE_CORREL
 									   INNER JOIN ESPACIO S ON R.ESP_CORREL = S.ESP_CORREL
 									   INNER JOIN EDIFICIO D ON S.EDI_CORREL = D.EDI_CORREL
 									   INNER JOIN ASISTE A ON E.EVE_CORREL = A.EVE_CORREL
 									   INNER JOIN USUARIO U ON A.USU_RUT = U.USU_RUT
-									   WHERE U.USU_RUT = "'.$rut.'"
-									     AND E.CAT_CORREL = '.$value1.'
-								         AND E.EST_CORREL = '.$value2.';');
+									   WHERE U.USU_RUT = '".$rut."'
+									     AND E.CAT_CORREL = ".$value1."
+								         AND E.EST_CORREL = ".$value2.";");
+
+
+	}
 
 		while($row = mysqli_fetch_array($result)){
 		?>
@@ -129,7 +81,6 @@
 			</div>
 		<?php
 		}
-	}
 
 	include("desconexion.php");
 ?>
